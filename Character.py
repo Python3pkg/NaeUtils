d261dccb6e41e26f4cbb2fd0fa1e5fdb068aac1b
Julien LEFEVRE <julien.lefevr@gmail.com>
2013-06-23 23:19:31 +0200

    Character displayer, use of import

--- Character.py
+++ Character.py
@@ -2,7 +2,14 @@ import math
 
 # A character in the Nae world
 class Character:
+
     def __init__(self):
+
+        # character description
+        self.sName = None
+        self.iAge = None
+
+        # main caracteristics
         self.iStrength = 0
         self.iAgility = 0 # agilite
         self.iMental = 0 # Mental
@@ -15,9 +22,9 @@ class Character:
         # deducted caracteristics
         self.dElementary = {}
 
-        self.iLifeMax = None# Stamina x 2 + Strength
+        self.iLifeMax = None # Stamina x 2 + Strength
         self.iNaergyMax = None #
-        self.iHealing = None# Stamina / 3 ceil down
+        self.iHealing = None # Stamina / 3 ceil down
         self.iWatering = None # Instinct / 8 number of naergic point per hour
         self.iTalent = None
         self.iNaergy = None
@@ -26,11 +33,34 @@ class Character:
         self.iPanache = None
 
 
+        # Counters
+        self.iMoney = 0
+
+
+    def getName(self):
+        return self.sName
+
+    def setName(self, sName):
+        self.sName = sName
+
+    def getAge(self):
+        return self.iAge
+
+    def setAge(self, iAge):
+        self.iAge = iAge
+
+
+    # This method allows to compute all secondaries and traits caracteristic from the primary
     def compute(self):
         self.__computeArdor()
         self.__computeLifeMax()
         self.__computeHealing()
         self.__computeWatering()
+        self.__computeBook()
+        self.__computeElementary()
+        self.__computeMuse()
+        self.__computeReflex()
+
 
     # base
 
@@ -129,9 +159,13 @@ class Character:
         self.iLifeMax = (self.iStamina * 2) + self.iStrength # force confirmee pour composante
 
     def __computeHealing(self):
+        if 0 == self.iStamina:
+            self.iHealing = 0
         self.iHealing = math.ceil(self.iStamina / 3)
 
     def __computeWatering(self):
+        if 0 == self.iWaterring:
+            self.iInstinct = 0
         self.iWaterring = math.ceil(self.iInstinct / 8)
 
     def __computeArdor(self):
@@ -155,3 +189,32 @@ class Character:
 
     def __computeTwoCaracAndDivisor(self, iFirstCarac, iSecond, iDivisor):
         return math.ceil((iFirstCarac + iSecond) / iDivisor)
+
+###############################
+##  DISPLAYER FOR CHARACTER  ##
+###############################
+from termcolor import colored
+
+class CharacterDisplayer:
+
+    def setCharacter(self, oCharacter):
+        assert isinstance(oCharacter, Character)
+        self.oCharacter = oCharacter
+
+    def display(self):
+        self.displayMainDescription()
+
+    def displayMainDescription(self):
+        sName = self.oCharacter.getName()
+        if None != sName:
+            self.displayMember('Name', sName)
+
+        iAge = self.oCharacter.getAge()
+        if None != iAge:
+            self.displayMember('Age', iAge)
+
+
+
+    def displayMember(self, sMemberName, mMemberValue):
+        print colored(sMemberName+' : ', 'green') + colored(mMemberValue, 'red')
+
